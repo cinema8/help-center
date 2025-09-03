b4d540e13fa765ef44be4036eb4125ad93804624
baris.erdemli <baris.erdemli@gobito.com>
2025-09-03 14:29:15 +0300

    SEO changes

--- src/lib/folderMeta.js
+++ src/lib/folderMeta.js
@@ -3,54 +3,81 @@ export const folderMeta = [
     slug: 'create-and-edit-videos',
     title: 'Create and edit videos',
     description: 'Get started easily with tools to upload, record and edit your videos.',
-    icon: '/icons/VideoCameraAddOutlined.svg'
+    icon: {
+      src: '/icons/VideoCameraAddOutlined.svg',
+      alt: 'Video camera add icon'
+    }
   },
   {
     slug: 'host-manage-and-customise',
     title: 'Host, manage and customise',
     description: 'Organise your library, manage permissions and customise features like the video player for brand alignment.',
-    icon: '/icons/PlaySquareOutlined.svg'
+    icon: {
+      src: '/icons/PlaySquareOutlined.svg',
+      alt: 'Play square icon'
+    }
   },
   {
     slug: 'engage-and-monetise',
     title: 'Engage and monetise',
     description: 'Add CTAs, forms and hotspots to turn your videos into sales funnels and generate leads.',
-    icon: '/icons/DollarCircleOutlined-1.svg'
+    icon: {
+      src: '/icons/DollarCircleOutlined-1.svg',
+      alt: 'Dollar circle icon'
+    }
   },
   {
     slug: 'design-advanced-interactivity',
     title: 'Design advanced interactivity',
     description: 'Video branching, gamification, shoppable videos and more with Interaction Designer.',
-    icon: '/icons/EditWithCreativeStudio.svg'
+    icon: {
+      src: '/icons/EditWithCreativeStudio.svg',
+      alt: 'Creative studio edit icon'
+    }
   },
   {
     slug: 'share-videos-securely',
     title: 'Share videos securely',
     description: 'Publish, embed and stream your videos with secure sharing.',
-    icon: '/icons/ShareAltOutlined.svg'
+    icon: {
+      src: '/icons/ShareAltOutlined.svg',
+      alt: 'Share icon'
+    }
   },
   {
     slug: 'analyze-and-optimise',
     title: 'Analyze and optimise',
     description: 'Track performance and use analytics to grow your video strategy and improve ROI.',
-    icon: '/icons/Analytics.svg'
+    icon: {
+      src: '/icons/Analytics.svg',
+      alt: 'Analytics chart icon'
+    }
   },
   {
     slug: 'integrations-and-third-party-platforms',
     title: 'Integrations and third-party platforms',
     description: 'Connect Cinema8 with LMS, CRM and ad platforms you already use for a streamlined workflow.',
-    icon: '/icons/ApiOutlined.svg'
+    icon: {
+      src: '/icons/ApiOutlined.svg',
+      alt: 'API integration icon'
+    }
   },
   {
     slug: 'advanced-tools-for-developers',
     title: 'Advanced tools for developers',
     description: 'Use APIs, webhooks and our widget environment to customise and scale your workflows.',
-    icon: '/icons/CodeOutlined.svg'
+    icon: {
+      src: '/icons/CodeOutlined.svg',
+      alt: 'Code developer tools icon'
+    }
   },
   {
     slug: 'accounts-and-billing',
     title: 'Accounts and billing',
     description: 'Manage subscriptions, payments and account settings.',
-    icon: '/icons/MyPlan.svg'
+    icon: {
+      src: '/icons/MyPlan.svg',
+      alt: 'User account and billing icon'
+    }
   }
 ];
