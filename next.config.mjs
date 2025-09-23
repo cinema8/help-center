c4768050d75415d1b5c7e44ac799526ed6651f87
inal.incedal <inal.incedal@gobito.com>
2025-09-23 18:01:47 +0100

    add some redirects

--- next.config.mjs
+++ next.config.mjs
@@ -8,30 +8,65 @@ const withNextra = nextra({
 export default withNextra({
   // ... Other Next.js config options
    images: {
-      remotePatterns: [
-        {
-          protocol: "http",
-          hostname: "127.0.0.1",
-          port: "1337",
-          pathname: "/uploads/**",
-        },
-        {
-          protocol: "http",
-          hostname: "127.0.0.1",
-          port: "9090",
-          pathname: "/**",
-        },
-        // Cinema8 assets
-        {
-          protocol: "https",
-          hostname: "cinema8.com",
-          pathname: "/**",
-        },
-        {
-          protocol: "https",
-          hostname: "www.cinema8.com",
-          pathname: "/**",
-        },
-      ],
-    },
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "127.0.0.1",
+        port: "1337",
+        pathname: "/uploads/**",
+      },
+      {
+        protocol: "http",
+        hostname: "127.0.0.1",
+        port: "9090",
+        pathname: "/**",
+      },
+      // Cinema8 assets
+      {
+        protocol: "https",
+        hostname: "cinema8.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "www.cinema8.com",
+        pathname: "/**",
+      },
+    ],
+  },
+  async redirects() {
+    return [
+      {
+        source: "/portal/:slug*",
+        destination: "/",
+        permanent: true
+      },
+      {
+        source: "/is-cinema8-compatible-with-embedly-api",
+        destination: "/",
+        permanent: true
+      },
+      {
+        source: "/conditional-action-element",
+        destination: "/",
+        permanent: true
+      },
+      {
+        source: "/use-cinema8-to-grow-your-videos",
+        destination: "/",
+        permanent: true
+      },
+      {
+        source: "/authentication-sso",
+        destination: "/",
+        permanent: true
+      },
+      {
+        source: "/element-properties-common-features",
+        destination: "/",
+        permanent: true
+      }      
+    ]
+    
+  },
 })
