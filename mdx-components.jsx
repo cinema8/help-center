5a52a9b306c70e75fe9a7d6662f729a1b1eebf2d
inal.incedal <inal.incedal@gobito.com>
2025-10-20 11:54:03 +0100

    override h2 and h3 tags for mdx-componentsg

--- mdx-components.jsx
+++ mdx-components.jsx
@@ -7,6 +7,54 @@ const themeComponents = getThemeComponents()
 export function useMDXComponents(components) {
   return {
     ...themeComponents,
-    ...components
+    ...components,
+    // override h2 tags
+    h2: ({ children, ...props }) => {
+      const text =
+        typeof children === 'string'
+          ? children
+          : Array.isArray(children)
+          ? children.join(' ')
+          : ''
+
+      const customId = text
+        .toLowerCase()
+        .replace(/[^\w]+/g, '-')
+        .replace(/(^-|-$)/g, '')
+
+      return (
+        <h2
+          id={customId}
+          className="x:tracking-tight x:text-slate-900 x:dark:text-slate-100 x:font-semibold x:target:animate-[fade-in_1.5s] x:mt-10 x:border-b x:pb-1 x:text-3xl nextra-border"
+          {...props}
+        >
+          {children}
+        </h2>
+      )
+    },
+    // override h3 tags
+    h3: ({ children, ...props }) => {
+      const text =
+        typeof children === 'string'
+          ? children
+          : Array.isArray(children)
+          ? children.join(' ')
+          : ''
+
+      const customId = text
+        .toLowerCase()
+        .replace(/[^\w]+/g, '-')
+        .replace(/(^-|-$)/g, '')
+
+      return (
+        <h2
+          id={customId}
+          className="x:tracking-tight x:text-slate-900 x:dark:text-slate-100 x:font-semibold x:target:animate-[fade-in_1.5s] x:mt-8 x:text-2xl"
+          {...props}
+        >
+          {children}
+        </h2>
+      )
+    }
   }
 }
