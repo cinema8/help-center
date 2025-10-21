cbaa218c48425f35867bc8729fcc8ced72f99a04
inal.incedal <inal.incedal@gobito.com>
2025-10-21 10:10:17 +0100

    override h4 tags

--- mdx-components.jsx
+++ mdx-components.jsx
@@ -47,14 +47,40 @@ export function useMDXComponents(components) {
         .replace(/(^-|-$)/g, '')
 
       return (
-        <h2
+        <h3
           id={customId}
           className="x:tracking-tight x:text-slate-900 x:dark:text-slate-100 x:font-semibold x:target:animate-[fade-in_1.5s] x:mt-8 x:text-2xl"
           {...props}
         >
           {children}
-        </h2>
+        </h3>
+      )
+    },
+    // override h4 tags
+    h4: ({ children, ...props }) => {
+      const text =
+        typeof children === 'string'
+          ? children
+          : Array.isArray(children)
+          ? children.join(' ')
+          : ''
+
+      const customId = text
+        .toLowerCase()
+        .replace(/[^\w]+/g, '-')
+        .replace(/(^-|-$)/g, '')
+
+      return (
+        <h4
+          id={customId}
+          className="x:tracking-tight x:text-slate-900 x:dark:text-slate-100 x:font-semibold x:target:animate-[fade-in_1.5s] x:mt-8 x:text-xl"
+          {...props}
+        >
+          {children}
+        </h4>
       )
     }
   }
 }
+
+
